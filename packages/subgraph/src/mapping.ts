5a5452efb791c8c4890548ca7e2af8c867e13353
thomas-waite <thomas.waite4@gmail.com>
2020-05-17 23:30:32 +0100

    feat(subgraph): setup for OptionStatus entity, with Create event

--- packages/subgraph/src/mapping.ts
+++ packages/subgraph/src/mapping.ts
@@ -1,22 +1,32 @@
-import { NewGravatar, UpdatedGravatar } from '../generated/Gravity/Gravity'
-import { Gravatar } from '../generated/schema'
+import { Create } from '../generated/Hack/Options';
+import { OptionStatus } from '../generated/schema';
 
-export function handleNewGravatar(event: NewGravatar): void {
-  let gravatar = new Gravatar(event.params.id.toHex())
-  gravatar.owner = event.params.owner
-  gravatar.displayName = event.params.displayName
-  gravatar.imageUrl = event.params.imageUrl
-  gravatar.save()
+export function handleNewOption(event: Create): void {
+  let option = new OptionStatus(event.params.optionId.toHex());
+  option.holder = event.params.account;
+  option.save();
 }
 
-export function handleUpdatedGravatar(event: UpdatedGravatar): void {
-  let id = event.params.id.toHex()
-  let gravatar = Gravatar.load(id)
-  if (gravatar == null) {
-    gravatar = new Gravatar(id)
-  }
-  gravatar.owner = event.params.owner
-  gravatar.displayName = event.params.displayName
-  gravatar.imageUrl = event.params.imageUrl
-  gravatar.save()
-}
+// export function handleExerciseOption(event: Exercise): void {
+//   let id = event.params.id.toHex()
+//   let gravatar = Gravatar.load(id)
+//   if (gravatar == null) {
+//     gravatar = new Gravatar(id)
+//   }
+//   gravatar.owner = event.params.owner
+//   gravatar.displayName = event.params.displayName
+//   gravatar.imageUrl = event.params.imageUrl
+//   gravatar.save()
+// }
+
+// export function handleExpireOption(event: Expire): void {
+//     let id = event.params.id.toHex()
+//     let gravatar = Gravatar.load(id)
+//     if (gravatar == null) {
+//       gravatar = new Gravatar(id)
+//     }
+//     gravatar.owner = event.params.owner
+//     gravatar.displayName = event.params.displayName
+//     gravatar.imageUrl = event.params.imageUrl
+//     gravatar.save()
+//   }
